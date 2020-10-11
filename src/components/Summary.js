9cd5c1ea15bd6eb68507e233fbad548b422fb708
Jeffrey Orndorff <jeffrey.orndorff@gmail.com>
2020-10-11 02:33:51 -0700

    Add resume link

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -4,18 +4,18 @@ import { Container } from "../theme/styles";
 
 ////////////// STYLED COMPONENTS ///////////////
 const P = styled.p`
-  font-size: ${pr => pr.theme.fontsizes.fs_p};
+  font-size: ${(pr) => pr.theme.fontsizes.fs_p};
   margin: 2.4rem 0;
   line-height: 1.2;
-`
+`;
 
 const A = styled.a`
-display: block;
+  display: block;
   text-decoration: underline;
-  font-size: ${pr => pr.theme.fontsizes.fs_p};
-  color: ${pr => pr.theme.colors.c_tertiary};
+  font-size: ${(pr) => pr.theme.fontsizes.fs_p};
+  color: ${(pr) => pr.theme.colors.c_tertiary};
   margin-bottom: 2rem;
-`
+`;
 //////////// END STYLED COMPONENTS /////////////
 
 /////////////// REACT COMPONENT ////////////////
@@ -23,13 +23,29 @@ export default function Summary() {
   return (
     <Container>
       <P>
-       I am a Full-Stack web developer who loves technology and its integration into everyday living. My passion is in helping others. I strive to accomplish this by finding creative solutions to every-day problems for businesses, educators, and those with impairments or disabilities. With a background in management and leadership roles, I understand the value that comes from working in a team. I am excited to work alongside others to make the world a better place.
+        I am a Full-Stack web developer who loves technology and its integration
+        into everyday living. My passion is in helping others. I strive to
+        accomplish this by finding creative solutions to every-day problems for
+        businesses, educators, and those with impairments or disabilities. With
+        a background in management and leadership roles, I understand the value
+        that comes from working in a team. I am excited to work alongside others
+        to make the world a better place.
       </P>
       <P>
-      While my strength is in JavaScript, I have spent the past 18 months studying data flow, algorithms, data structures, time & space complexities, and other computer science concepts that allow me to transfer my coding knowledge across multiple languages. Over the past 4 months, I have picked up and learned OOP through Python3 and Java.
+        While my strength is in JavaScript, I have spent the past 18 months
+        studying data flow, algorithms, data structures, time & space
+        complexities, and other computer science concepts that allow me to
+        transfer my coding knowledge across multiple languages. Over the past 4
+        months, I have picked up and learned OOP through Python3 and Java.
       </P>
 
-      <A href="#">Download my resume</A>
+      <A
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://drive.google.com/file/d/1lCLQyx_lGsrm7S1Z1nCNa-7aBQODDP16"
+      >
+        View my resume
+      </A>
     </Container>
   );
 }
